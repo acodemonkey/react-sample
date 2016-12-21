c9b15f79d93e9dfed63b04089f0c12f1f9bbbd70
Brittany Vance <brit@bnparsons.info>
2016-12-21 14:35:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Hello React</title>
-  </head>
-  <body>
-    <div id="hello"></div>
-    <div id="world"></div>
-  </body>
-</html>
+import Hello from './hello.jsx';
+import World from './world.jsx';
