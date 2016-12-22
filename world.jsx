f549d0cebc53023930677b97f2c3e6f9a82d1a32
Brittany Vance <brit@bnparsons.info>
2016-12-21 22:19:24 -0500

    Update world.jsx

--- world.jsx
+++ world.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello</h1>
-  }
+class World extends React.Component {
+  render() {
+    return <h1>World</h1>
+  }
 }
+
+ReactDOM.render(<World/>, document.getElementById('world'));
